6ca1bc4801785454894a6ec9562c5042747ff89b
RogueAstro <leonardoags@usp.br>
2015-10-01 22:10:30 -0300

    tweaks to facilitate fine tuning

--- find_vsini.py
+++ find_vsini.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from pwoogs import moog,estimate,utils
+from powers import moog,estimate,utils
 import q2
 import shutil as sh
 
@@ -10,7 +10,7 @@ import shutil as sh
  Find v sin(i) with Python and MOOG synth
 
 This code is used to estimate the projected rotation of solar twin stars
-using Python, MOOG synth, in addition to RogueAstro's pwoogs and 
+using Python, MOOG synth, in addition to RogueAstro's powers and
 astroChasqui's q2 codes.
 
 Required files (soon I will commit some example files):
@@ -26,6 +26,7 @@ star_names = np.loadtxt('s_twins.csv',
                         usecols=(0,),
                         dtype=str,
                         delimiter=',')
+N = len(star_names)
 
 # This is used to manage data arrays
 u = utils.arr_manage()
@@ -39,11 +40,11 @@ def set_star(choice, **kwargs):
         invert = kwargs['inverted_filelist']
     else:
         invert = False
-            
+
     choice = int(choice)
-    
+
     print 'Creating the stellar atmosphere file.'
-    
+
     data = np.loadtxt('s_twins.csv',
                       usecols=(1,2,3,4),
                       skiprows=1,
@@ -58,7 +59,7 @@ def set_star(choice, **kwargs):
         f.truncate()
         f.write(
             """KURTYPE
-    %.1f/  %.2f/  %.2f      mic =  %.2f                                         
+    %.1f/  %.2f/  %.2f      mic =  %.2f
                 %i
     5000.0\n""" % (data[choice,0],
                    data[choice,1],
@@ -86,7 +87,7 @@ def set_star(choice, **kwargs):
     6.1     7.1     8.1   12.1  20.1  22.1  23.1  26.1  40.1
     """ % data[choice,2]
                 )
-    
+
     # filenames.lis contains the names of all the fits files of the spectra
     # In my case, the list is in an opposite order as the list of star names,
     # so that's choice is re-set if invert == True
@@ -96,8 +97,8 @@ def set_star(choice, **kwargs):
     print "Creating the spectrum_full.dat file for %s" % filename[choice]
     sh.copyfile(filename[choice],'spectrum_full.dat')
 
-# Next, there is the code that finds the v sin(i) in fully automatic mode, for 
-# a specific star. v_m returns the macroturbulent velocity for a solar twin 
+# Next, there is the code that finds the v sin(i) in fully automatic mode, for
+# a specific star. v_m returns the macroturbulent velocity for a solar twin
 # with a temperature T.
 
 def v_m(T):
@@ -110,42 +111,58 @@ def manage(choice, interval, lines, chunk):
     spec_window = np.array([lines[choice,0]-interval/2,lines[choice,0]+interval/2])
     u.cut(spec_window[0]-chunk,spec_window[1]+chunk,'spectrum_full.dat','spectrum.dat')
     print 'Done.\n'
-    
+
     return spec_window
 
 # Function that returns the corrections factors for line center and continuum
 def correct(choice, data, lines, cont_type, r_1, r_2, r_3):
 
     # The following lines are used to find calibration corrections for the spectral line
+    # If the correction is too off for 10 tries, it failsafes to no-correction at all (wl_shift=0)
+    max_num_tries = 10
     print 'Finding the shift on the wavelength.'
     wl_shift = 10.
     ind = u.find_index(lines[choice,0],data[:,0])
-    while abs(wl_shift) > 1.0:
+    tries = 0
+    max_shift_allowed = 1.0
+    while abs(wl_shift) > max_shift_allowed and tries < max_num_tries:
         center = u.find_center(data[ind-r_1+1:ind+r_1+2])
         wl_shift = lines[choice,0]-center
+        tries += 1
+    if tries >= max_num_tries-1:
+        print 'Wavelength shift estimation failed. Failsafing to 0.0'
+        wl_shift = 0.0
     print 'Wavelength shift = %.4f\n' % wl_shift
 
-    print "Finding the correction factor for the continuum."
-    ind_min = u.find_index(lines[choice,0]-r_2,data[:,0])
-    ind_max = u.find_index(lines[choice,0]+r_2,data[:,0])
-    if cont_type == 'single':    
-        corr = 1.0/np.mean(u.find_corr(
-            data[ind_min:ind_max,:],
+    max_corr_allowed = 0.1
+    tries = 0
+    corr = 2.0
+    while abs(1.0-corr) > max_corr_allowed and tries < max_num_tries:
+        print "Finding the correction factor for the continuum."
+        ind_min = u.find_index(lines[choice,0]-r_2,data[:,0])
+        ind_max = u.find_index(lines[choice,0]+r_2,data[:,0])
+        if cont_type == 'single':
+            corr = 1.0/np.mean(u.find_corr(
+                data[ind_min:ind_max,:],
+                r_3
+                ))
+        elif cont_type == 'multi':
+            target_wls = np.loadtxt('continuum.dat')
+            corr = 1.0/np.mean(u.find_corr_from_ensemble(
+            data[ind_min-r_3:ind_max+r_3,:],
+            target_wls[choice,:],
             r_3
             ))
-    elif cont_type == 'multi':
-        target_wls = np.loadtxt('continuum.dat')
-        corr = 1.0/np.mean(u.find_corr_from_ensemble(
-        data[ind_min:ind_max,:],
-        target_wls[choice,:],
-        r_3
-        ))
+        tries += 1
+    if tries >= max_num_tries-1:
+        print 'Continuum correction failed. Failsafing to 1.0'
+        corr = 1.0
     print "Correction factor = %.4f" % corr
 
     return wl_shift, corr
 
-def full_auto(choice, interval, res_power, SN, **kwargs):
-    
+def full_auto(star, choice, interval, res_power, SN, **kwargs):
+
     # Spectrum chunk size. Default = 10. angstroms
     if ('chunk' in kwargs):
         chunk = kwargs['chunk']
@@ -159,7 +176,7 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
         assert cont_type == 'multi', 'Continuum correction type invalid'
     else:
         cont_type = 'single'
-        
+
     # Radius of points to be used in finding the correction for the line center
     # Default = 3
     if ('r_1' in kwargs):
@@ -167,7 +184,7 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
         assert radius_1 > 0, 'Invalid radius for line center correction'
     else:
         radius_1 = 3
-        
+
     # Radius in angstroms for the region around the target wavelength to be
     # analyzed for the continuum . Default = 3.0
     if ('r_2' in kwargs):
@@ -175,15 +192,15 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
         assert radius_2 > 0, 'Invalid radius of wavelength region'
     else:
         radius_2 = 3.0
-        
+
     # Radius in points to be used in finding the correction for the continuum.
     # Default = 2
     if ('r_3' in kwargs):
         radius_3 = kwargs['r_3']
         assert radius_3 > 0, 'Invalid radius for continuum correction'
     else:
-        radius_3 = 2    
-    
+        radius_3 = 2
+
     # Radius in points to be used in evaluating the performance function
     # Default = 7
     if ('r_4' in kwargs):
@@ -191,14 +208,14 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
         assert radius_4 > 0, 'Invalid radius for performance evaluation'
     else:
         radius_4 = 7
-        
+
     # Blue wing weight to be used on estimation. Default = 10.0
     if ('bw' in kwargs):
         bw = kwargs['bw']
         assert bw >= 0.0, 'Invalid weight for blue wing'
     else:
         bw = 10.0
-        
+
     # Red wing weight to be used on estimation. Default = 5.0
     if ('rw' in kwargs):
         rw = kwargs['rw']
@@ -212,21 +229,33 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
         assert cw >= 0.0, 'Invalid weight for line center'
     else:
         cw = 25.0
-        
+
     # Bad fit tolerance in number of points above the S/N ratio. Default = 2
     if ('tol' in kwargs):
         tol = kwargs['tol']
         assert tol >= 0, 'Invalid tolerance'
     else:
         tol = 2
-                    
+
     # 'plot' on window or 'save' as png? Default = plot on window
     if ('output' in kwargs):
         output = kwargs['output']
         assert output == 'save', 'Invalid radius for continuum correction'
     else:
         output = 'plot'
-    
+
+    # optimize will estimate vsini automatically. Default = True
+    if ('optimize' in kwargs):
+        optimize = kwargs['optimize']
+    else:
+        optimize = True
+
+    # guess is a numpy.array([vsini,abund]), useful if optimize != True
+    if ('guess' in kwargs):
+        guess = kwargs['guess']
+    else:
+        guess = np.array([1.0, 0.0])
+
     choice = int(choice)
 
     # Synthesis parameters
@@ -239,19 +268,17 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
     T_star = star_info[0]
     v_macro = v_m(T_star)
 
-    data = np.loadtxt('spectrum.dat')
-    
     # Managing the data file
     spec_window = manage(choice, interval, lines, chunk)
+
+    data = np.loadtxt('spectrum.dat')
 
     # The instrumental broadening
     gauss = np.mean(spec_window)/res_power
 
     # Finding the correction factors
-    wl_shift, corr = correct(choice, data, lines, cont_type, radius_1, radius_2, 
+    wl_shift, corr = correct(choice, data, lines, cont_type, radius_1, radius_2,
                              radius_3)
-    
-    print "Now starting estimation of vsini..."
 
     # Instatiating the function to write parameters for MOOG
     r = estimate.vsini(
@@ -268,38 +295,58 @@ def full_auto(choice, interval, res_power, SN, **kwargs):
                 perf_radius=radius_4,
                 SN=SN,
                 badfit_tol = tol,
-                star_name=star_names[m]
+                star_name=star_names[star]
                 )
 
     if output == 'plot':
         save = 'window'
     else:
-        save = '%s_line%i.png'%(star_names[m],choice)
-    
-    # Finding vsini and abundance
-    vsini,abund,bfs = r.find(N=15,
-                             max_i=20,
-                             min_i=10,
-                             limits=[0.05,0.001],
-                             save=save)
-    return vsini,abund,bfs
+        save = '%s_line%i.png'%(star_names[star],choice)
+
+    if optimize == True:
+        print "Now starting estimation of vsini..."
+        vsini,abund,bfs = r.find(N=15,
+                                max_i=20,
+                                min_i=10,
+                                limits=[0.05,0.001],
+                                save=save)
+        return vsini,abund,bfs
+    else:
+        print 'Running MOOG with the following values:'
+        print 'vsini = %.2f' % guess[0]
+        print 'abund = %.3f' % guess[1]
+        r.write_params(guess[0],guess[1])
+        moog.run(star_name = star_names[star])
+
 
 # The next functions are specifically used in my research, but they can be used
 # as examples for other setups
 
 def batch(filename):
     with open(filename,'w') as r:
-        for m in range(87):
+        for m in range(N):
             star = m
             set_star(star, inverted_filelist=True)
             v = np.zeros(6,float)
             a = np.zeros(6,float)
             bf = np.empty(6,bool)
             for i in range(6):
-                v[i],a[i],bf[i] = full_auto(i,
-                                            interval=1.0, 
-                                            res_power=65000., 
-                                            SN=400)
+                v[i],a[i],bf[i] = full_auto(star,
+                                            i,
+                                            interval=1.0,
+                                            res_power=65000.,
+                                            SN=400,
+                                            continuum_correction='multi',
+                                            #r_1 = 3,
+                                            r_2 = 1.0,
+                                            r_3 = 3,
+                                            r_4 = 8,
+                                            bw = 10.,
+                                            rw = 5.,
+                                            cw = 15.,
+                                            tol = 2,
+                                            output = 'save'
+                                            )
             print '%s: median(vsini) = %.1f' % (star_names[star],np.median(v))
             r.write('\n%s\t' % star_names[star])
             for k in range(6):
@@ -308,25 +355,35 @@ def batch(filename):
                 r.write('%.3f\t' % a[k])
             for k in range(6):
                 r.write('%s\t' % bf[k])
-                
-def single_star(m):
-    star = m
+
+def single_star(star):
     set_star(star, inverted_filelist=True)
     v = np.zeros(6,float)
     a = np.zeros(6,float)
     bf = np.empty(6,bool)
     for i in range(6):
-        v[i],a[i],bf[i] = full_auto(i,
-                                    interval=1.0, 
-                                    res_power=65000., 
-                                    SN=400)
+        v[i],a[i],bf[i] = full_auto(star,
+                                    i,
+                                    interval=1.0,
+                                    res_power=65000.,
+                                    continuum_correction='multi',
+                                    SN=400,
+                                    r_1 = 3,
+                                    r_2 = 1.0,
+                                    r_3 = 3,
+                                    r_4 = 8,
+                                    bw = 10.,
+                                    rw = 5.,
+                                    cw = 15.,
+                                    tol = 2,
+                                    output = 'save')
     print '%s: median(vsini) = %.1f' % (star_names[star],np.median(v))
-    
+
 def single_line(m,l):
     star = m
     set_star(star, inverted_filelist=True)
     v, a, bf = full_auto(l,
-                         interval=1.0, 
-                         res_power=65000., 
+                         interval=1.0,
+                         res_power=65000.,
                          SN=400)
     print '%s: vsini = %.1f' % (star_names[star],np.median(v))
